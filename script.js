eb077fa2bfb21697867ac69eab0872562e9f7d4b
ashish <ashishrajpoudel28@gmail.com>
2025-06-06 16:03:18 +0545

    removing ./

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const closeModal = document.querySelector('.close-modal');
   
   // Load projects from JSON file
-  fetch('./projects.json')
+  fetch('projects.json')
     .then(response => response.json())
     .then(data => {
       projects = data.projects;
