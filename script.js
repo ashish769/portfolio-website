30d13c8013e6ce5e5c3c604ca7d288e15e4d5b3d
ashish <ashishrajpoudel28@gmail.com>
2025-06-06 15:19:39 +0545

    making the projects section dynamic 6th

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const closeModal = document.querySelector('.close-modal');
   
   // Load projects from JSON file
-  fetch('https://www.ashishrajpoudel.com.np/projects.json')
+  fetch('./projects.json')
     .then(response => response.json())
     .then(data => {
       projects = data.projects;
